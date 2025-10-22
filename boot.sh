42402e08f18bf507fb1a066dcc938ac0791d7277
Lukas Chleba <chlebik@gmail.com>
2025-10-23 01:03:16 +0200

    Change Omarchy to Airchy and initial removing some packages

--- boot.sh
+++ boot.sh
@@ -3,16 +3,16 @@
 # Set install mode to online since boot.sh is used for curl installations
 export OMARCHY_ONLINE_INSTALL=true
 
-ansi_art='                 ▄▄▄                                                   
- ▄█████▄    ▄███████████▄    ▄███████   ▄███████   ▄███████   ▄█   █▄    ▄█   █▄ 
-███   ███  ███   ███   ███  ███   ███  ███   ███  ███   ███  ███   ███  ███   ███
-███   ███  ███   ███   ███  ███   ███  ███   ███  ███   █▀   ███   ███  ███   ███
-███   ███  ███   ███   ███ ▄███▄▄▄███ ▄███▄▄▄██▀  ███       ▄███▄▄▄███▄ ███▄▄▄███
-███   ███  ███   ███   ███ ▀███▀▀▀███ ▀███▀▀▀▀    ███      ▀▀███▀▀▀███  ▀▀▀▀▀▀███
-███   ███  ███   ███   ███  ███   ███ ██████████  ███   █▄   ███   ███  ▄██   ███
-███   ███  ███   ███   ███  ███   ███  ███   ███  ███   ███  ███   ███  ███   ███
- ▀█████▀    ▀█   ███   █▀   ███   █▀   ███   ███  ███████▀   ███   █▀    ▀█████▀ 
-                                       ███   █▀                                  '
+ansi_art='
+   ▄████████  ▄█     ▄████████  ▄████████    ▄█    █▄    ▄██   ▄
+  ███    ███ ███    ███    ███ ███    ███   ███    ███   ███   ██▄
+  ███    ███ ███▌   ███    ███ ███    █▀    ███    ███   ███▄▄▄███
+  ███    ███ ███▌  ▄███▄▄▄▄██▀ ███         ▄███▄▄▄▄███▄▄ ▀▀▀▀▀▀███
+▀███████████ ███▌ ▀▀███▀▀▀▀▀   ███        ▀▀███▀▀▀▀███▀  ▄██   ███
+  ███    ███ ███  ▀███████████ ███    █▄    ███    ███   ███   ███
+  ███    ███ ███    ███    ███ ███    ███   ███    ███   ███   ███
+  ███    █▀  █▀     ███    ███ ████████▀    ███    █▀     ▀█████▀
+                    ███    ███                                   '
 
 clear
 echo -e "\n$ansi_art\n"
@@ -22,7 +22,7 @@ sudo pacman -Syu --noconfirm --needed git
 # Use custom repo if specified, otherwise default to basecamp/omarchy
 OMARCHY_REPO="${OMARCHY_REPO:-basecamp/omarchy}"
 
-echo -e "\nCloning Omarchy from: https://github.com/${OMARCHY_REPO}.git"
+echo -e "\nCloning Airchy from: https://github.com/${OMARCHY_REPO}.git"
 rm -rf ~/.local/share/omarchy/
 git clone "https://github.com/${OMARCHY_REPO}.git" ~/.local/share/omarchy >/dev/null
 
